0ba556453a831a544a1e53928a4b57c504b2eb4e
Adya Mohanty <am2558@cornell.edu>
2024-03-22 14:01:28 -0400

    convert jaccardsim to def

--- jaccardsim.py
+++ jaccardsim.py
@@ -25,67 +25,66 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-    
+def search(words): 
 
-count_vect = CountVectorizer()
+	count_vect = CountVectorizer()
+
+	pubmed = PubMed(tool="PubSearch", email="yjc22@cornell.edu")
+
+	q = words
+	query =  '(("1970/01/01"[Date - Create] : "2040"[Date - Create])) AND '+words
+
+	results = pubmed.query(query, max_results=10)
+
+	dflist = []
+
+	resultdf = pd.DataFrame([item.toDict() for item in results])
+	resultdf = resultdf[resultdf.abstract.notna()]
+
+	dflist.append(resultdf)
+	df = pd.concat(dflist)
+
+	d = df.drop_duplicates(subset = ['abstract'], keep = 'last').reset_index(drop = True)
+
+	corpus = []
+	articleMap = {}
+	corpus.append(q)
 
 
-pubmed = PubMed(tool="PubSearch", email="yjc22@cornell.edu")
+	for index, article in df.iterrows():
+		abstract = article['abstract']
+		title = article['title']
+		authors = article['authors']
+		ids = article['pubmed_id']
+		firstID = ""
+		rest = ""
 
-q = "what compounds promote mitochondrial function?"
-query =  '(("1970/01/01"[Date - Create] : "2040"[Date - Create])) AND '+q
+		if '\n' in ids:
+			firstID, rest = ids.split('\n', 1)
+		else:
+			firstID = ids
 
-results = pubmed.query(query, max_results=10)
+		#print(firstID)
+		link = "https://pubmed.ncbi.nlm.nih.gov/"+firstID+"/"
 
-dflist = []
+		corpus.append(abstract)
+		articleMap[abstract] = (title,link)
 
-resultdf = pd.DataFrame([item.toDict() for item in results])
-resultdf = resultdf[resultdf.abstract.notna()]
+	X_train_counts = count_vect.fit_transform(corpus)
 
-dflist.append(resultdf)
-df = pd.concat(dflist)
+	pd.DataFrame(X_train_counts.toarray(),columns=count_vect.get_feature_names_out())
 
-d = df.drop_duplicates(subset = ['abstract'], keep = 'last').reset_index(drop = True)
+	sims = []
+	for a in range(1, len(corpus)):
+		sims.append(jaccard_score(X_train_counts[0].toarray()[0], X_train_counts[a].toarray()[0], average = 'macro'))
 
-corpus = []
-articleMap = {}
-corpus.append(q)
+	sorter = []
 
+	for a in range(1,len(corpus)):
+		sorter.append((sims[a-1],articleMap[corpus[a]][0],articleMap[corpus[a]][1],corpus[a]))
 
-for index, article in df.iterrows():
-	abstract = article['abstract']
-	title = article['title']
-	authors = article['authors']
-	ids = article['pubmed_id']
-	firstID = ""
-	rest = ""
+	sorted_array = sorted(sorter)
 
-	if '\n' in ids:
-	    firstID, rest = ids.split('\n', 1)
-	else:
-	    firstID = ids
-
-	#print(firstID)
-	link = "https://pubmed.ncbi.nlm.nih.gov/"+firstID+"/"
-
-	corpus.append(abstract)
-	articleMap[abstract] = (title,link)
-
-X_train_counts = count_vect.fit_transform(corpus)
-
-pd.DataFrame(X_train_counts.toarray(),columns=count_vect.get_feature_names_out())
-
-sims = []
-for a in range(1, len(corpus)):
-	sims.append(jaccard_score(X_train_counts[0].toarray()[0], X_train_counts[a].toarray()[0], average = 'macro'))
-
-sorter = []
-
-for a in range(1,len(corpus)):
-	sorter.append((sims[a-1],articleMap[corpus[a]][0],articleMap[corpus[a]][1],corpus[a]))
-
-sorted_array = sorted(sorter)
-
-print("The top results are:")
-for a in range(len(sorted_array)):
-	print(a+1,": "+sorted_array[a][1]+" "+sorted_array[a][2])
+	results = []
+	for a in range(len(sorted_array)):
+		results.append((a+1,": "+sorted_array[a][1]+" "+sorted_array[a][2]))
