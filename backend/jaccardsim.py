b7a0f7444d55a7738b71863ea66b250656614174
Victoria <vb272@cornell.edu>
2024-03-25 21:37:02 -0400

    Update jaccardsim.py
    only returns non duplicate abstracts now

--- backend/jaccardsim.py
+++ backend/jaccardsim.py
@@ -9,91 +9,104 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 # Route to handle incoming data
-@app.route('/send-data', methods=['POST'])
+@app.route("/send-data", methods=["POST"])
 def receive_data():
     # Get data from the request
     data = request.get_json()
 
-
     # Return a response
     return jsonify(processed_data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
 
 
-def search(words): 
+def search(words):
 
-	count_vect = CountVectorizer()
+    count_vect = CountVectorizer()
 
-	pubmed = PubMed(tool="PubSearch", email="yjc22@cornell.edu")
+    pubmed = PubMed(tool="PubSearch", email="yjc22@cornell.edu")
 
-	q = words
-	query =  '(("1970/01/01"[Date - Create] : "2040"[Date - Create])) AND '+words
+    q = words
+    query = '(("1970/01/01"[Date - Create] : "2040"[Date - Create])) AND ' + words
 
-	results = pubmed.query(query, max_results=10)
+    results = pubmed.query(query, max_results=10)
 
-	dflist = []
+    dflist = []
 
-	resultdf = pd.DataFrame([item.toDict() for item in results])
-	#resultdf = resultdf[resultdf.abstract.notna()]
+    resultdf = pd.DataFrame([item.toDict() for item in results])
+    # resultdf = resultdf[resultdf.abstract.notna()]
 
-	dflist.append(resultdf)
-	df = pd.concat(dflist)
+    dflist.append(resultdf)
+    df = pd.concat(dflist)
 
-	#d = df.drop_duplicates(subset = ['abstract'], keep = 'last').reset_index(drop = True)
+    # d = df.drop_duplicates(subset = ['abstract'], keep = 'last').reset_index(drop = True)
 
-	corpus = []
-	articleMap = {}
-	corpus.append(q)
+    corpus = []
+    articleMap = {}
+    corpus.append(q)
 
+    for index, article in df.iterrows():
+        abstract = article["abstract"]
+        title = article["title"]
+        authors = article["authors"]
+        ids = article["pubmed_id"]
+        firstID = ""
+        rest = ""
 
-	for index, article in df.iterrows():
-		abstract = article['abstract']
-		title = article['title']
-		authors = article['authors']
-		ids = article['pubmed_id']
-		firstID = ""
-		rest = ""
+        if "\n" in ids:
+            firstID, rest = ids.split("\n", 1)
+        else:
+            firstID = ids
 
-		if '\n' in ids:
-			firstID, rest = ids.split('\n', 1)
-		else:
-			firstID = ids
+        # print(firstID)
+        link = "https://pubmed.ncbi.nlm.nih.gov/" + firstID + "/"
 
-		#print(firstID)
-		link = "https://pubmed.ncbi.nlm.nih.gov/"+firstID+"/"
+        print("title:", title)
+        print("abstract:", abstract)
+        if abstract == None:
+            abstract = ""
+        corpus.append(abstract)
+        articleMap[abstract] = (title, link)
 
-		print("title:", title)
-		print("abstract:",abstract)
-		if abstract == None:
-			abstract = ''
-		corpus.append(abstract)
-		articleMap[abstract] = (title,link)
+    X_train_counts = count_vect.fit_transform(corpus)
 
+    print("WE MADE IT")
 
-	X_train_counts = count_vect.fit_transform(corpus)
+    pd.DataFrame(X_train_counts.toarray(), columns=count_vect.get_feature_names_out())
 
-	print("WE MADE IT")
+    sims = []
+    for a in range(1, len(corpus)):
+        sims.append(
+            jaccard_score(
+                X_train_counts[0].toarray()[0],
+                X_train_counts[a].toarray()[0],
+                average="macro",
+            )
+        )
 
-	pd.DataFrame(X_train_counts.toarray(),columns=count_vect.get_feature_names_out())
+    sorter = []
 
-	sims = []
-	for a in range(1, len(corpus)):
-		sims.append(jaccard_score(X_train_counts[0].toarray()[0], X_train_counts[a].toarray()[0], average = 'macro'))
+    for a in range(1, len(corpus)):
+        sorter.append(
+            (
+                sims[a - 1],
+                articleMap[corpus[a]][0],
+                articleMap[corpus[a]][1],
+                corpus[a],
+            )
+        )
 
-	sorter = []
+    sorted_array = sorted(sorter)
 
-	for a in range(1,len(corpus)):
-		sorter.append((sims[a-1],articleMap[corpus[a]][0],articleMap[corpus[a]][1],corpus[a]))
+    results = []
+    for a in range(len(sorted_array)):
+        if (sorted_array[a][1] + " " + sorted_array[a][2]) not in results:
+            results.append((sorted_array[a][1] + " " + sorted_array[a][2]))
+        # results.append({'result': sorted_array[a][1] + " " + sorted_array[a][2]})
 
-	sorted_array = sorted(sorter)
-
-	results = []
-	for a in range(len(sorted_array)):
-		results.append((sorted_array[a][1]+" "+sorted_array[a][2]))
-		#results.append({'result': sorted_array[a][1] + " " + sorted_array[a][2]})
-
-	print("DONE with JACCARD")
-	return results
+    print("DONE with JACCARD")
+    return results
