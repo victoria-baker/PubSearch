00a481daf049f787923752a4ef77c5d6294f2499
Adya Mohanty <am2558@cornell.edu>
2024-04-24 15:59:13 -0400

    minor spacing indentation changes

--- backend/cosinesim.py
+++ backend/cosinesim.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-#from sklearn.metrics import jaccard_score
+# from sklearn.metrics import jaccard_score
 from flask import Flask, request, jsonify
 import time
 import concurrent.futures
@@ -19,20 +19,23 @@ app = Flask(__name__)
 isRocchio = False
 
 # Route to handle incoming data
+
+
 @app.route('/send-data', methods=['POST'])
 def receive_data():
     # Get data from the request
     data = request.get_json()
 
-
     # Return a response
     return jsonify(processed_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 Entrez.email = "vb272@cornell.edu"
+
 
 def process_article(article, corpus, articleMap):
     abstract = article["abstract"]
@@ -52,22 +55,26 @@ def process_article(article, corpus, articleMap):
 
     link = "https://pubmed.ncbi.nlm.nih.gov/" + firstID + "/"
 
-    if abstract == None:
+    if abstract is None:
         abstract = ""
     corpus.append(abstract)
-    
+
     articleMap[abstract] = (title, link, firstID)
-    
+
+
 def process_articles_multithread(df, corpus, articleMap):
     threads = []
     for index, article in df.iterrows():
-        thread = threading.Thread(target=process_article, args=(article, corpus, articleMap))
+        thread = threading.Thread(
+    target=process_article, args=(
+        article, corpus, articleMap))
         thread.start()
         threads.append(thread)
-        #time.sleep(0.4) # change?
+        # time.sleep(0.4) # change?
 
     for thread in threads:
         thread.join()
+
 
 def search(words):
     start = time.perf_counter()
@@ -76,13 +83,12 @@ def search(words):
     pubmed = PubMed(tool="PubSearch", email="yjc22@cornell.edu")
     q = words
     query = (
-        '(("1970/01/01"[Date - Create] : "2023"[Date - Create])) AND ffrft[Filter] AND hasabstract AND '
-        + words
-    )
+    '(("1970/01/01"[Date - Create] : "2023"[Date - Create])) AND ffrft[Filter] AND hasabstract AND ' +
+     words)
 
     results = pubmed.query(query, max_results=50)
-    #start1 = time.perf_counter()
-    #print(start1-start)
+    # start1 = time.perf_counter()
+    # print(start1-start)
 
     dflist = []
 
@@ -91,19 +97,22 @@ def search(words):
     dflist.append(resultdf)
     df = pd.concat(dflist)
 
-    df = df.drop_duplicates(subset=["abstract"], keep="last").reset_index(drop=True)
+    df = df.drop_duplicates(
+    subset=["abstract"],
+    keep="last").reset_index(
+        drop=True)
 
     corpus = []
     articleMap = OrderedDict()
-    #citations_count = {}
+    # citations_count = {}
     corpus.append(q)
-    #added = time.perf_counter()
-    #print(added-start)
+    # added = time.perf_counter()
+    # print(added-start)
 
     process_articles_multithread(df, corpus, articleMap)
 
-    #end = time.perf_counter()
-    #print(end-start)
+    # end = time.perf_counter()
+    # print(end-start)
     X_train_counts = count_vect.fit_transform(corpus)
 
     if X_train_counts.shape[0] > 1:
@@ -114,19 +123,19 @@ def search(words):
         threshold = 0.5  # Set your threshold
         important_indices = np.where(singular_values > threshold)[0]
 
-        X_filtered = np.dot(
-            X_svd[:, important_indices], np.diag(singular_values[important_indices])
-        )
+        X_filtered = np.dot(X_svd[:, important_indices], np.diag(
+            singular_values[important_indices]))
 
         query_vector = count_vect.transform([words])
         query_svd = svd.transform(query_vector)
-        query_filtered = np.dot(
-            query_svd[:, important_indices], np.diag(singular_values[important_indices])
-        )
+        query_filtered = np.dot(query_svd[:, important_indices], np.diag(
+            singular_values[important_indices]))
 
-        #end2 = time.perf_counter()
-        #print(end2-start)
-        similarity = cosine_similarity(query_filtered.reshape(1, -1), X_filtered)
+        # end2 = time.perf_counter()
+        # print(end2-start)
+        similarity = cosine_similarity(
+    query_filtered.reshape(
+        1, -1), X_filtered)
 
         sims = []
         for a in range(1, len(similarity[0])):
@@ -143,23 +152,27 @@ def search(words):
                     articleMap[a][1],
                     a,
                     articleMap[a][2],
-                    #citations_count[a],
+                    # citations_count[a],
                 )
             )
             ctr += 1
 
         sorted_array = sorted(sorter)
 
-	results = []
-		for a in range(min(len(sorted_array),5)):
-			abstract_lines = sorted_array[a][3].split(".")
-			abstract_final = ""
-			for i in range(min(10, len(abstract_lines))):
-				abstract_final += abstract_lines[i]
-			results.append((sorted_array[a][1]+"@"+sorted_array[a][2]+"@"+abstract_final+".."))
-			print((sorted_array[a][1]+"@"+sorted_array[a][2])+"\n")
-		return results
-	else:
-		return []
-
-	
+        results = []
+        for a in range(min(len(sorted_array), 5)):
+            abstract_lines = sorted_array[a][3].split(".")
+            abstract_final = ""
+            for i in range(min(10, len(abstract_lines))):
+                abstract_final += abstract_lines[i]
+            results.append(
+    (sorted_array[a][1] +
+    "@" +
+        sorted_array[a][2] +
+        "@" +
+        abstract_final +
+         ".."))
+        print((sorted_array[a][1] + "@" + sorted_array[a][2]) + "\n")
+        return results
+    else:
+        return []
