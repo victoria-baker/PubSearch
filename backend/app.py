0841a6afe29a8c77a1dfcbe35db223229755e90e
Adya Mohanty <am2558@cornell.edu>
2024-04-24 15:42:58 -0400

    change to jaccard sim as search function

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
 from cosinesim import search as cosine_search
+from jaccardsim import search as jaccard_search
 
 # ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
@@ -37,7 +38,7 @@ def receive_data():
     search_query = data["data"]
     print("STARTING")
     print(search_query)
-    results = cosine_search(search_query)
+    results = jaccard_search(search_query)
     print("RESULTS", results)
     # Format results as needed before sending back to the client
     return results
--- backend/cosinesim.py
+++ backend/cosinesim.py
@@ -5,11 +5,12 @@ import numpy as np
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics import jaccard_score
+#from sklearn.metrics import jaccard_score
 from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
+isRocchio = False
 
 # Route to handle incoming data
 @app.route('/send-data', methods=['POST'])
@@ -104,18 +105,69 @@ def search(words):
 			sorter.append((sims[a-1],articleMap[corpus[a]][0],articleMap[corpus[a]][1],corpus[a]))
 
 		sorted_array = sorted(sorter)
+		return sorted_array
+	# 	results = []
+	# 	for a in range(min(len(sorted_array),5)):
+	# 		abstract_lines = sorted_array[a][3].split(".") ##THIS IS WHERE THE ABSTRACT IS STORED
+	# 		abstract_final = ""
+	# 		for i in range(min(10, len(abstract_lines))):
+	# 			abstract_final += abstract_lines[i]
+	# 		results.append((sorted_array[a][1]+"@"+sorted_array[a][2]+"@"+abstract_final+".."))
+	# 		print((sorted_array[a][1]+"@"+sorted_array[a][2])+"\n")
+	# 	return results
+	# else:
+	# 	return []
 
+def rocchio(words, relevant_indices, irrelevant_indices):
+	# Create a CountVectorizer object to get the term vectors
+	count_vect = CountVectorizer()
+	# Get the abstract for each relevant and irrelevant document
+	sorted_array = search(words)
+	abstracts = []
+	for a in range(min(len(sorted_array),5)):
+		abstracts.append(sorted_array[a][3]) ##THIS IS WHERE THE ABSTRACT IS STORED
 
-		results = []
-		for a in range(min(len(sorted_array),5)):
-			abstract_lines = sorted_array[a][3].split(".")
-			abstract_final = ""
-			for i in range(min(10, len(abstract_lines))):
-				abstract_final += abstract_lines[i]
-			results.append((sorted_array[a][1]+"@"+sorted_array[a][2]+"@"+abstract_final+".."))
-			print((sorted_array[a][1]+"@"+sorted_array[a][2])+"\n")
-		return results
-	else:
-		return []
-
+	relevant_abstracts = [abstracts[i] for i in relevant_indices]
+	irrelevant_abstracts = [abstracts[i] for i in irrelevant_indices]
 	
+	# Calculate term vectors based on the query
+	query_vector = count_vect.transform([words]).toarray()[0]
+	
+	# Calculate term vectors for relevant and irrelevant documents
+	relevant_vectors = [count_vect.transform([abstract]).toarray()[0] for abstract in relevant_abstracts]
+	irrelevant_vectors = [count_vect.transform([abstract]).toarray()[0] for abstract in irrelevant_abstracts]
+	
+	# Calculate the centroid of the relevant and irrelevant documents
+	relevant_centroid = np.mean(relevant_vectors, axis=0)
+	irrelevant_centroid = np.mean(irrelevant_vectors, axis=0)
+	
+	# Calculate the new query vector based on the Rocchio algorithm
+	alpha = 1.0  # Weight for the original query vector
+	beta = 0.8  # Weight for the relevant centroid vector
+	gamma = 0.2  # Weight for the irrelevant centroid vector
+	
+	new_query_vector = alpha * query_vector + beta * relevant_centroid - gamma * irrelevant_centroid
+	
+	# Convert the new query vector back to a string query
+	new_query = ' '.join([word for word, count in zip(count_vect.get_feature_names(), new_query_vector) if count > 0])
+	
+	# Perform the search with the new query
+	results = search(new_query)
+	
+	return results
+ 
+
+
+##helper to return the top 5 results
+def getTop5(query):
+	sorted_results = search(query)
+
+	results = []
+	for a in range(min(len(sorted_results),5)):
+		abstract_lines = sorted_results[a][3].split(".") ##THIS IS WHERE THE ABSTRACT IS STORED
+		abstract_final = ""
+		for i in range(min(10, len(abstract_lines))):
+			abstract_final += abstract_lines[i]
+		results.append((sorted_results[a][1]+"@"+ sorted_results[a][2]+"@"+abstract_final+".."))
+		print((sorted_results[a][1]+"@"+ sorted_results [a][2])+"\n")
+	return results
